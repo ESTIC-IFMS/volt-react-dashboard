1de1cb8dfd1776a7b12c8bd9fac8af084e751004
exodojaffar <exodojaffar@gmail.com>
2021-03-03 17:39:26 -0400

    update: adiciona props para variação do texto
    Modifica para ser modularizado e implementado em outras partes.

--- src/reciclecode/FormHeader.js
+++ src/reciclecode/FormHeader.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default () => {
+export default ({text}) => {
   return (
     <div className="text-center text-md-center mb-4 mt-md-0">
-      <h3 className="mb-0">Crie uma conta</h3>
+      <h3 className="mb-0">{text}</h3>
     </div>
   )
 }
