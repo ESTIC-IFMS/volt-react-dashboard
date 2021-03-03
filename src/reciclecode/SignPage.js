1e42909276d2d243ca0a06ffeed1c9378c19f89d
exodojaffar <exodojaffar@gmail.com>
2021-03-02 20:47:11 -0400

    add: compomente para modularização das paginas de sign(*)
    Havia codigo repetido, sabe o que fazemos com codigo repitido?
    Nos modularizamos.

---
+++ src/reciclecode/SignPage.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
+import { Col, Row, Card, Button, Container } from '@themesberg/react-bootstrap';
+
+import { Routes } from "../routes";
+import BgImage from "../assets/img/illustrations/signin.svg";
+
+export default ({restoPagina})=>{
+	return (
+		<main>
+			<section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
+				<Container>
+					<p className="text-center">
+						<Card.Link as={Link} to={Routes.DashboardOverview.path} className="text-gray-700">
+							<FontAwesomeIcon icon={faAngleLeft} className="me-2" /> Back to homepage
+						</Card.Link>
+					</p>
+
+					<Row className="justify-content-center form-bg-image" style={{ backgroundImage: `url(${BgImage})` }}>
+						<Col xs={12} className="d-flex align-items-center justify-content-center">
+
+						{restoPagina()}
+
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		</main>
+	)
+}
