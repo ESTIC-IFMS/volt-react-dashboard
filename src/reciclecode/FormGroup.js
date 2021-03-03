800d44b2d6a77f9ff34b8cf19566db32d4b84e47
exodojaffar <exodojaffar@gmail.com>
2021-03-02 20:48:34 -0400

    fix: pacote FontAwasomeIcon faltante

--- src/reciclecode/FormGroup.js
+++ src/reciclecode/FormGroup.js
@@ -1,9 +1,11 @@
 import React from "react";
+
 import {Form, InputGroup } from '@themesberg/react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default ({formProps}) =>{
   return (
-    <Form.Group id={formProps.id} className={`mb-4` + formProps.className}>
+    <Form.Group id={formProps.id} className={`mb-4 ${formProps.className}`}>
       <Form.Label>{formProps.textLabel}</Form.Label>
 
       <InputGroup>
