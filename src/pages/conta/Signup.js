888e9098470043c7e0f6cbfd167df61874ae7588
exodojaffar <exodojaffar@gmail.com>
2021-03-04 10:50:47 -0400

    update: termina os campos do formulario de cadastro
    Adiciona os campos:
    - username
    - user_type (fixo)
    - user_id  (fixo)
    - email
    - telefone
    - data de nascimento
    - sexo
    - estado
    - cidade
    - formação academica
    - siap
    - escola
    - senha

--- src/pages/conta/Signup.js
+++ src/pages/conta/Signup.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
 // import { faFacebookF, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import {Form, Card, Button, FormCheck } from '@themesberg/react-bootstrap';
+import {Form, Card, Button, FormCheck, InputGroup} from '@themesberg/react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import { Routes } from "../../routes";
@@ -11,32 +11,10 @@ import FormInput from "../../reciclecode/FormInput.js";
 import FormHeader from "../../reciclecode/FormHeader.js";
 import SignPage from "../../reciclecode/SignPage.js";
 
-import formsInputData from './SignupForm.js'
-/*
-Outros signup/login
-
-Não sei se vai usar
-
-<div className="mt-3 mb-4 text-center">
-    <span className="fw-normal">or</span>
-</div>
-<div className="d-flex justify-content-center my-4">
-    <Button variant="outline-light" className="btn-icon-only btn-pill text-facebook me-2">
-        <FontAwesomeIcon icon={faFacebookF} />
-    </Button>
-    <Button variant="outline-light" className="btn-icon-only btn-pill text-twitter me-2">
-        <FontAwesomeIcon icon={faTwitter} />
-    </Button>
-    <Button variant="outline-light" className="btn-icon-only btn-pil text-dark">
-        <FontAwesomeIcon icon={faGithub} />
-    </Button>
-</div>
-*/
-
-// Dados para criar cada campo do form
+// Posteriormente, transformar a senha, confirmar senha em um componente
 
 const teste = (e) => {
-  console.log(e)
+  console.log(e.target.value)
 }
 
 const restoPagina = () => {
@@ -46,26 +24,153 @@ const restoPagina = () => {
   <FormHeader/>
 
   <Form className="mt-4" onSubmit={teste}>
-    {
-      formsInputData.map(
-        props => {
-          return (<FormInput formProps={props} />   )
-        }
-      )
-    }                      
 
-    <Form.Group id="sex" className="mb-4">
-      <Form.Label>Sexo</Form.Label>
-
-      <Form.Select>
-        <option defaultValue>Selecione...</option>
-        <option>Masculino</option>
-        <option>Feminino</option>
-
-        <Form.Control required />
-      </Form.Select>
-
+    <Form.Group>
+      <Form.Control type='hidden' value='1' name='user_type'/>
     </Form.Group>
+
+    <Form.Group>
+      <Form.Control type='hidden' value='1' name='user_id'/>
+    </Form.Group>
+
+    <FormInput id='name' className='' label='Nome Completo' inputComponent={
+      () => {
+        return(
+          <InputGroup>
+            <Form.Control required type='text' name='username' placeholder='Nome Completo' onChange={teste} />
+          </InputGroup>
+        )
+      }
+    } />
+
+    <FormInput id='email' className='' label='Email' inputComponent={
+      () => {
+        return(
+          <InputGroup>
+            <Form.Control required type='text' name='email' placeholder='professor@email.com' onChange={teste} />
+          </InputGroup>
+        )
+      }
+    } />
+
+    <FormInput id='phone' className='' label='Telefone/Celular' inputComponent={
+      () => {
+        return(
+          <InputGroup>
+            <Form.Control required type='tel' name='phone' min='9' max='12' placeholder='67 9999-9999' onChange={teste} />
+          </InputGroup>
+        )
+      }
+    } />
+
+    <FormInput id='birthdate' className='' label='Data de Nascimento' inputComponent={
+      () => {
+        return(
+          <InputGroup>
+            <Form.Control required type='date' name='birthdate' onChange={teste} />
+          </InputGroup>
+        )
+      }
+    } />
+
+    <FormInput id='sex' className='' label='Sexo' inputComponent={
+      () => {
+        return (
+          <Form.Select required name='genre' onChange={teste}>
+            <option defaultValue value='0'>Selecione...</option>
+            <option value='Masculino'>Masculino</option>
+            <option value='Feminino'>Feminino</option>
+          </Form.Select>    
+        )
+      }
+    }/>
+
+    <FormInput id='state' className='' label='Estado' inputComponent={
+      () => {
+        return (
+          <InputGroup>
+           <Form.Control required type='text' name='state' onChange={teste} />
+          </InputGroup>
+        )
+      }
+    }/>
+
+    <FormInput id='city' className='' label='Cidade' inputComponent={
+      () => {
+        return (
+          <InputGroup>
+           <Form.Control required type='text' name='city' onChange={teste} />
+          </InputGroup>
+        )
+      }
+    }/>
+
+    <FormInput id='academic_degree' className='' label='Formação Academica' inputComponent={
+      () => {
+        return (
+          <Form.Select required name='academic_degree' onChange={teste}>
+            <option defaultValue value='0'>Selecione...</option>
+            <option value='Graduação'>Graduação</option>
+            <option value='Mestrado'>Mestrado</option>
+            <option value='Doutorado'>Doutorado</option>
+          </Form.Select>    
+        )
+      }
+    }/>
+
+    <FormInput id='siap' className='' label='SIAP' inputComponent={
+      () => {
+        return (
+          <InputGroup>
+           <Form.Control required type='text' name='siap' onChange={teste} />
+          </InputGroup>
+        )
+      }
+    }/>
+
+    <FormInput id='school' className='' label='Escola' inputComponent={
+      () => {
+        return (
+          <Form.Select required name='school_id' onChange={teste}>
+            <option defaultValue value='0'>Selecione...</option>
+            <option value={1}>Escola Tal Tal</option>
+            <option value={2}>Escola de Tal Tel</option>
+          </Form.Select>    
+        )
+      }
+    }/>
+
+
+
+    <FormInput id='password' className='' label='Sua Senha' inputComponent={
+      () => {
+        return (
+          <InputGroup>
+            <Form.Control required type="password" placeholder="Senha" />
+          </InputGroup>   
+        )
+      }
+    }/>
+
+    <FormInput id='password_confirm' className='' label='Confirme sua senha' inputComponent={
+      () => {
+        return (
+          <InputGroup>
+            <Form.Control required type="password" placeholder="Senha" />
+          </InputGroup>   
+        )
+      }
+    }/>
+
+    <FormInput id='teacher_code' className='' label='Codigo do Professor' inputComponent={
+      () => {
+        return (
+          <InputGroup>
+           <Form.Control required type="text" placeholder="CODIGO" />
+          </InputGroup>   
+        )
+      }
+    }/>
 
     <FormCheck type="checkbox" className="d-flex mb-4">
       <FormCheck.Input required id="terms" className="me-2" />
@@ -74,7 +179,7 @@ const restoPagina = () => {
       </FormCheck.Label>
     </FormCheck>
 
-    <Button variant="primary" type="submit" className="w-100" onSubmit={teste}>
+    <Button variant="primary" type="submit" className="w-100">
       Sign up
     </Button>
   </Form>
